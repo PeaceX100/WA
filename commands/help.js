883d7fef9cc3c86a64bf5cde63f8ec763d8a75cb
PeaceX100 <146706811+PeaceX100@users.noreply.github.com>
2024-02-20 15:20:04 +0000

    why

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,12 @@
+const { MessageMedia } = require('whatsapp-web.js');
+
 module.exports = {
     name: 'help',
     aliases: ['h','menu','m'],
     description: 'Need help? look no further',
     
     execute: async (whatsapp, message, args) => {
+      const media = MessageMedia.fromFilePath('././');
       await whatsapp.sendMessage(message.from, `*☆↦﹝ALAN'S HELP MENU﹞↤☆*\n\n✹《 _FUN COMMANDS_ 》✹\n*1. 8ball*\n*2. Coinflip*\n*3. RPS(Rock,Paper,Scissors)*,\n*4. Say*\n\n✹《 _UPLIFTING COMMANDS_ 》✹\n*1. Motivate*\n*2. Quote*\n\n✹《 _UTILITY COMMANDS_ 》✹\n*1. Calculate*\n*2. Dictionary*\n*3. Help*\n*4. Ping*`);
     }
   };
